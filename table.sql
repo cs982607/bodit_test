59b192e9b8a4352336f52b0ef7d882bc23370010
정현석 <hjs@b2groups.com>
2022-10-15 19:10:00 +0900

    테이블 설계 변경

--- table.sql
+++ table.sql
@@ -1,14 +1,15 @@
-
 -- User Table Create SQL
 CREATE TABLE User
 (
-	user_id       INT         NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '회원ID',
-    birth_date    DATE        NOT NULL    COMMENT '생년월일', 
-    height        INT         NOT NULL    COMMENT '키', 
-    phone         INT         NOT NULL    COMMENT '전화번호', 
-    use_YN        TINYINT     NOT NULL    COMMENT '사용여부', 
-    create_time   DATETIME    NOT NULL    COMMENT '가입일', 
-    update_time   DATETIME    NOT NULL    COMMENT '수정일' 
+    user_id      VARCHAR(20)      NOT NULL    COMMENT '회원ID', 
+    birth_date   DATE             NOT NULL    COMMENT '생년월일', 
+    height       DECIMAL(3, 1)    NOT NULL    COMMENT '키', 
+    phone        VARCHAR(13)      NOT NULL    COMMENT '전화번호', 
+    USE_YN       VARCHAR(1)       NOT NULL DEFAULT'Y' COMMENT '사용여부', 
+    create_time  DATETIME         NOT NULL    COMMENT '가입일', 
+    update_time  DATETIME         NOT NULL    COMMENT '수정일', 
+    name         VARCHAR(20)      NOT NULL    COMMENT '이름', 
+    PRIMARY KEY (user_id)
 );
 
 ALTER TABLE User COMMENT '회원정보 테이블';
@@ -17,28 +18,52 @@ ALTER TABLE User COMMENT '회원정보 테이블';
 -- MeasurementHistory Table Create SQL
 CREATE TABLE MeasurementHistory
 (
-    history_id   INT         NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '측정 기록ID', 
-    create_time  DATETIME    NOT NULL    COMMENT '측정 날짜', 
-    weight       INT         NOT NULL    COMMENT '측정 몸무게', 
-    user_id      INT         NOT NULL    COMMENT '회원ID',
-	FOREIGN KEY (user_id)
-    REFERENCES User(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+    ms_id        VARCHAR(20)      NOT NULL    COMMENT '측정 기록ID', 
+    create_time  DATETIME         NOT NULL    COMMENT '측정 날짜', 
+    weight       DECIMAL(3, 1)    NOT NULL    COMMENT '측정 몸무게', 
+    user_id      VARCHAR(20)      NOT NULL    COMMENT '회원ID', 
+    PRIMARY KEY (ms_id)
 );
 
 ALTER TABLE MeasurementHistory COMMENT '회원 측정 기록 테이블';
+
+ALTER TABLE MeasurementHistory
+    ADD CONSTRAINT FK_MeasurementHistory_user_id_User_user_id FOREIGN KEY (user_id)
+        REFERENCES User (user_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+-- MeasurementCode Table Create SQL
+CREATE TABLE MeasurementCode
+(
+    code_id    VARCHAR(20)    NOT NULL    COMMENT '측정코드ID', 
+    ms_name    VARCHAR(50)    NOT NULL    COMMENT '종류', 
+    min_value  INT            NOT NULL    COMMENT '최소 수치', 
+    max_value  INT            NOT NULL    COMMENT '최대 수치', 
+    USE_YN     VARCHAR(1)     NOT NULL DEFAULT'Y' COMMENT '사용여부', 
+    PRIMARY KEY (code_id)
+);
+
+ALTER TABLE MeasurementCode COMMENT '측정 코드 테이블';
 
 
 -- MeasurementData Table Create SQL
 CREATE TABLE MeasurementData
 (
-    data_id     INT            NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '측정 데이터ID', 
-    type        VARCHAR(20)    NOT NULL    COMMENT '종류', 
-    value       INT            NOT NULL    COMMENT '수치', 
-    history_id  INT            NOT NULL    COMMENT '측정 기록ID', 
-    use_YN      TINYINT        NOT NULL    COMMENT '사용여부', 
-	FOREIGN KEY (history_id)
-    REFERENCES MeasurementHistory(history_id) ON DELETE CASCADE ON UPDATE CASCADE
+    data_id  VARCHAR(20)    NOT NULL    COMMENT '측정 데이터ID', 
+    code_id  VARCHAR(20)    NOT NULL    COMMENT '측정코드ID', 
+    ms_id    VARCHAR(20)    NOT NULL    COMMENT '측정 기록ID', 
+    USE_YN   VARCHAR(1)     NOT NULL DEFAULT'Y' COMMENT '사용여부', 
+    PRIMARY KEY (data_id)
 );
 
 ALTER TABLE MeasurementData COMMENT '회원 측정 데이터 테이블';
+
+ALTER TABLE MeasurementData
+    ADD CONSTRAINT FK_MeasurementData_ms_id_MeasurementHistory_ms_id FOREIGN KEY (ms_id)
+        REFERENCES MeasurementHistory (ms_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE MeasurementData
+    ADD CONSTRAINT FK_MeasurementData_code_id_MeasurementCode_code_id FOREIGN KEY (code_id)
+        REFERENCES MeasurementCode (code_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
 
